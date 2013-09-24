c6b8745e292c0d314102587ddfb83cff923936a0
Szuturon <ronnyszutu@gmail.com>
2013-09-24 17:33:57 -0400

    The even/odd reporter

--- js_fundamentals/forloops.js
+++ js_fundamentals/forloops.js
@@ -4,6 +4,16 @@
 
 // Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
 
+for (var i = 0; i <= 20; i++){
+	var str = i + " is ";
+	if (i % 2 == 0){
+		console.log(str + "even");
+	}
+	else{
+		console.log(str + "odd");
+	}
+	
+}
 // EXERCISE: Multiplication Tables
 
 // Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
