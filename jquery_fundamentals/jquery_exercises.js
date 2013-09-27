c6f38dda90fef4457bafe7b64d03978c7eaf1284
Szuturon <ronnyszutu@gmail.com>
2013-09-27 00:35:16 -0400

    Select A Option in A Select Box

--- jquery_fundamentals/jquery_exercises.js
+++ jquery_fundamentals/jquery_exercises.js
@@ -59,18 +59,18 @@
 // Move "#child" from "#parent1" to "#parent2"
 // Hint: you can use .appendTo
 
-$(document).ready(function(){
-	$('div#child').appendTo('div#parent2')
-});
+// $(document).ready(function(){
+// 	$('div#child').appendTo('div#parent2')
+// });
 
 // EXERCISE: Select A Option in A Select Box
 
 // Select the second option in the selectbox by script
 // Hint: you will have to select the option and can use .eq and .attr
 
-// $(document).ready(function(){
-
-// });
+$(document).ready(function(){
+	$('option').eq(1).attr('selected', true)
+});
 
 // EXERCISE: Change Size
 
